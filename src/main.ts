93c9729a3d9eb1c4c355d8032bc52990ef91457b
liuyi <13403111884@163.com>
2021-12-28 18:39:39 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import './registerServiceWorker'
+import API from './api'
+import route from './router'
+import store from './store'
 
-createApp(App).use(store).use(router).mount('#app');
+const { routes, beforeEach, afterEach } = route
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to: any, from: any, next: any) => {
+  beforeEach(to, from, next)
+})
+
+router.afterEach(() => {
+  afterEach()
+})
+
+createApp(App)
+  .use(API)
+  .use(router)
+  .use(store)
+  .mount('#app')
